02cde57511d4b2d7cd45c6dfc4e461a5a87efd28
Javier <jevargasf@gmail.com>
2026-01-16 23:21:40 -0300

    form login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,21 @@
+<x-layouts.app>
+
+    <h2 class="text-center mt-3">Iniciar Sesión</h2>
+    <div class="card p-4 mx-auto my-5 text-center" id="formLogin">
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+
+            <div class="mb-3">
+                <label for="correo" class="form-label">Correo electrónico</label>
+                <input type="email" class="form-control" id="correo" placeholder="correo@ejemplo.com">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+
+            <button class="btn btn-primary" type="submit">Entrar</button>
+        </form>
+    </div>
+</x-layouts.app>
