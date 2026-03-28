a5bf789ed37cc06b0cd0d86c6acf5629692fc15b
Javier <jevargasf@gmail.com>
2026-03-28 20:05:55 -0300

    debug navbar

--- resources/views/components/nav/blog-nav.blade.php
+++ resources/views/components/nav/blog-nav.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand" href="{{ route('blog-personal.index') }}">
             ~/LHMQMQ
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar">
