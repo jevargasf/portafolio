51816f7999004af92d7dbe70308d7864da92e181
Javier <jevargasf@gmail.com>
2026-03-30 16:43:37 -0300

    sidebar responsive panel de administración

--- resources/views/components/layouts/panel.blade.php
+++ resources/views/components/layouts/panel.blade.php
@@ -13,10 +13,10 @@
 </head>
 <body>
     <div class="row m-0 min-vh-100">
-        <div class="col-2 m-0 p-0">
+        <div class="d-md-flex col-12 col-md-2 m-0 p-0">
             <x-nav.panel-sidebar/>
         </div>
-        <main class="col-10 m-0 py-0 px-4">
+        <main class="col-10 m-0 py-0 px-4 pb-5">
             {{ $slot }}
         </main>
     </div>
