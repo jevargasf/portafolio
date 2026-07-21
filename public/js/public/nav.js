a918c7e0d624d1e58fa49db78a98c0068ac17e56
Javier <jevargasf@gmail.com>
2026-07-21 14:16:34 -0400

    quitar js próximamente

--- public/js/public/nav.js
+++
@@ -1,7 +0,0 @@
-const nav = document.getElementById('nav');
-const btnToggler = document.getElementById('btnToggler');
-
-btnToggler.addEventListener('click', (e) => {
-    nav.classList.toggle('nav__collapse--open');
-
-});
